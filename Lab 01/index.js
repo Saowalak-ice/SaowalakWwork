199f35d24d97a1045b22930bbb999c17f80ed5e5
Saowalak-ice <Saowalak.w@psru.ac.th>
2019-12-07 12:41:02 +0700

    initial commit

---
+++ Lab 01/index.js
@@ -0,0 +1,24 @@
+console.log("Saowalak");
+//alert("กรุณากรอกตัวเลข");
+let firstName = "Youngjae";
+let lastName = "Choi";
+
+let age = 5;
+let height = 0.1;
+
+
+function squareNumber(number)
+{
+    return number*number;
+}
+
+    let result = squareNumber(7);
+
+
+function salary(num,hour)
+{
+    return num*hour;
+}
+    let resultsalary = salary(3,3240);
+
+
